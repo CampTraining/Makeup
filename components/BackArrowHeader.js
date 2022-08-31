d79127d3c8c88820646249a5bacac143700345de
adyhatem65 <adyetman65@gmail.com>
2022-08-31 12:47:52 +0200

    Added New_Pass Screen | Add Axios to ForgetPass Screen

--- components/BackArrowHeader.js
+++ components/BackArrowHeader.js
@@ -5,14 +5,11 @@ import {StylesForgetPass} from '../screens/forgetPass/styles';
 import {COLOR, ICONS} from '../constants';
 import {RFValue} from 'react-native-responsive-fontsize';
 export function BackArrowHeader(props) {
-  const {navigation} = props;
+  const {onPress} = props;
   return (
     <>
       <View style={[StylesForgetPass.arrowContainer]}>
-        <TouchableOpacity
-          onPress={() => {
-            navigation.navigate('Login');
-          }}>
+        <TouchableOpacity onPress={onPress}>
           <FontAwesome5
             name="chevron-left"
             color={COLOR.black}
